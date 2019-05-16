f54e5ad11ebff10c3e6bd8b753c2cadfd1313e77
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-16 15:10:00 -0400

    The initial properties for this table.

--- EFTesting/Models/SupportTicket.cs
+++ EFTesting/Models/SupportTicket.cs
@@ -1,11 +1,17 @@
+using System;
 using System.Collections.Generic;
 namespace EFTesting.Models
 {
-    public class SupportTicket
+    public partial class SupportTicket
     {
         public SupportTicket()
         {
             SupportLog = new HashSet<SupportLog>();   
         }
+        public int SupportTicketId { get; set; }
+        public DateTime DateReported { get; set; }
+        public DateTime? DateResolved { get; set; }
+        public string IssueDescription { get; set; }
+
     }
 }
