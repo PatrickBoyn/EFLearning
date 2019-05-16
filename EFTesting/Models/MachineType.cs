1a89fb9ede6d8d21dee38ffa3982045ca288a87d
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-16 12:10:57 -0400

    Added the initial model for this class.

--- EFTesting/Models/MachineType.cs
+++ EFTesting/Models/MachineType.cs
@@ -1,10 +1,15 @@
+using System.Collections.Generic;
 namespace EFTesting.Models
 {
     public partial class MachineType
     {
         public MachineType()
         {
-            
+            Machine = new HashSet<Machine>();
         }
+
+        public int MachineTypeId { get; set; }
+        public string Description { get; set; }
+        public ICollection<Machine> Machine { get; set; }
     }
 }
