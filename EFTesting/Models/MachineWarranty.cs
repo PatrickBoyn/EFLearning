bd33492d7c58425dba4ca87f2b6301663b67fad3
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-16 14:43:08 -0400

    The initial model for this class.

--- EFTesting/Models/MachineWarranty.cs
+++ EFTesting/Models/MachineWarranty.cs
@@ -1,7 +1,14 @@
+using System;
+
 namespace EFTesting.Models
 {
     public class MachineWarranty
     {
-        
+        public int MachineWarrantyId { get; set; }
+        public string ServiceTag { get; set; }
+        public DateTime WarrantyExpiration { get; set; }
+        public int MachineId { get; set; }
+        public int WarrantyProviderId { get; set; }
+        public WarrantyProvider WarrantyProvider { get; set; }
     }
 }
