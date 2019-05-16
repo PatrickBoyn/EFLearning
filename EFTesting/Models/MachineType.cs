5fcf3b3ceae836b8f771e8ee742458433406f656
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-16 14:49:48 -0400

    The ctor implimentation.

--- EFTesting/Models/MachineType.cs
+++ EFTesting/Models/MachineType.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
 namespace EFTesting.Models
 {
     public class MachineType
     {
-        
+        public MachineType()
+        {
+            Machine = new HashSet<Machine>();
+        }
     }
 }
