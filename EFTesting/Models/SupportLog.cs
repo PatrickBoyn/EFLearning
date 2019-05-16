42559f9554d8e61b34b876753dfbbeed80ac5953
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-16 15:13:42 -0400

    The initial properties of the table.

--- EFTesting/Models/SupportLog.cs
+++ EFTesting/Models/SupportLog.cs
@@ -1,7 +1,14 @@
+using System;
+
 namespace EFTesting.Models
 {
-    public class SupportLog
+    public partial class SupportLog
     {
-        
+        public int SupportLogId { get; set; }
+        public DateTime SupportLogEntryDate { get; set; }
+        public string SupportLogEntry { get; set; }
+        public string SupportLogUpdatedBy { get; set; }
+        public int SupportTicketId { get; set; }
+        public SupportTicket SupportTicket { get; set; }
     }
 }
