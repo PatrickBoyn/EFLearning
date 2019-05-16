6aba24def005c03cac4df7519eb35608f282e5f4
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-16 14:48:16 -0400

    The initial model for this table.

--- EFTesting/Models/OperatingSys.cs
+++ EFTesting/Models/OperatingSys.cs
@@ -1,10 +1,15 @@
+using System.Collections.Generic;
 namespace EFTesting.Models
 {
     public class OperatingSys
     {
         public OperatingSys()
         {
-            
+            Machine = new HashSet<Machine>();
         }
+        public int OperatingSysId { get; set; }
+        public string Name { get; set; }
+        public bool StillSupported { get; set; }
+        public ICollection<Machine> Machine { get; set; }
     }
 }
