85c451b99ce9b1af55e46bd75084f0cdf2bb35b2
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-16 17:05:11 -0400

    Added the initiasl propsets.

--- EFTesting/Data/MachineContext.cs
+++ EFTesting/Data/MachineContext.cs
@@ -1,7 +1,17 @@
+using EFTesting.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace EFTesting.Data
 {
-    public class MachineContext
+    class MachineContext : DbContext
     {
-        
+        public DbSet<Machine> Machine { get; set; }
+        public DbSet<MachineType> MachineType { get; set; }
+        public DbSet<MachineWarranty> MachineWarranty { get; set; }
+        public DbSet<OperatingSys> OperatingSys { get; set; }
+        public DbSet<SupportLog> SupportLog { get; set; }
+        public DbSet<SupportTicket> SupportTicket { get; set; }
+        public DbSet<WarrantyProvider> WarrantyProvider { get; set; }
+
     }
 }
