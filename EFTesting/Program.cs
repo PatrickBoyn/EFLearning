db96211faa00e1a0a6ee4d6b551342b239e6e072
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-18 16:38:32 -0400

    Added the initial variables and a do while loop.

--- EFTesting/Program.cs
+++ EFTesting/Program.cs
@@ -62,6 +62,13 @@ namespace EFTesting
 
         static void DataEntryMenu()
         {
+            ConsoleKeyInfo _consoleKeyInfo;
+            int result = -1;
+            bool cont = false;
+            do
+            {
+                
+            } while (true);
         }
     }
 }
